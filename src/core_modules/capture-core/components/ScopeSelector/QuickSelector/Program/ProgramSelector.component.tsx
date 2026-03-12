608fe9df32fdea26528f6bc7e279e4270e9527d3
Carlos Djedje <cdjedje18@gmail.com>
2026-03-12 14:27:34 +0200

    feat: custom program filter based on datastore

--- src/core_modules/capture-core/components/ScopeSelector/QuickSelector/Program/ProgramSelector.component.tsx
+++ src/core_modules/capture-core/components/ScopeSelector/QuickSelector/Program/ProgramSelector.component.tsx
@@ -92,11 +92,11 @@ const ProgramSelectorPlain = ({
     })
 
     useEffect(() => {
-        setProgramsArray(Array.from(programCollection.values()));
+        // setProgramsArray(Array.from(programCollection.values()));
 
-        // if (!selectedProgramId) {
-        //     programsDataStoreQuery.refetch()
-        // }
+        if (!selectedProgramId) {
+            programsDataStoreQuery.refetch()
+        }
 
     }, []);
 
